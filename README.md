0944e64fd5aabb9db05f995b9ee6b2d788f292c9
jack.taylor <jack.taylor@skybettingandgaming.com>
2021-08-21 16:18:38 +0100

    add connector and task status

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ A simple <span style="color:#3a9025">Con</span>nector M<span style="color:#3a902
 > conan list
 
 CONNECTORS
-connector1
-connector2
+
+connector1   RUNNING
+    Task  0  RUNNING      12.12.12.12:8083
+    Task  1  RUNNING      12.12.13.13:8083
+
+connector2   RUNNING
+    Task  0  RUNNING      12.12.12.12:8083
 ```
--- cmd/list.go
+++ cmd/list.go
@@ -18,12 +18,35 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
+	"os"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
+
+const connectorStateTemplate = `{{ .Name }}  {{ .Connector.State }}
+{{ range $task := .Tasks }}    Task {{ printf "%2d" $task.Id }}  {{ $task.State }}      {{ $task.WorkerId }}
+{{ end }}`
+
+type ConnectorStatus struct {
+	Name      string
+	Connector ConnectorState
+	Tasks     []TaskState
+}
+
+type ConnectorState struct {
+	State    string
+	WorkerId string `json:"worker_id"`
+}
+
+type TaskState struct {
+	Id       int
+	State    string
+	WorkerId string `json:"worker_id"`
+}
 
 // listCmd represents the list command
 var listCmd = &cobra.Command{
@@ -34,10 +57,13 @@ var listCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		host, port = GetPersistentFlags(cmd)
 		connectors := GetConnectors(host, port)
+		t := template.Must(template.New("").Parse(connectorStateTemplate))
 
 		fmt.Println("CONNECTORS")
+
 		for _, connector := range connectors {
-			fmt.Println(connector)
+			status := GetConnectorStatus(host, port, connector)
+			t.Execute(os.Stdout, status)
 		}
 	},
 }
@@ -54,6 +80,20 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func GetConnectorStatus(host string, port string, connector string) ConnectorStatus {
+	statusUrl := fmt.Sprintf("http://%s:%s/connectors/%s/status", host, port, connector)
+	log.Debug("getting connector status using URL: ", statusUrl)
+	resp, err := http.Get(statusUrl)
+	cobra.CheckErr(err)
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	cobra.CheckErr(err)
+
+	var status ConnectorStatus
+	json.Unmarshal(bodyBytes, &status)
+	return status
 }
 
 func GetConnectors(host string, port string) []string {
